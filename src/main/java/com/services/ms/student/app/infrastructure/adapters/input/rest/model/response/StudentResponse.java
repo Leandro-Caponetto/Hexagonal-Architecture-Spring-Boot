d5690d39604b5bcf9e7d64c636649e359e03b198
Leandro-Caponetto <caponettopeppers@gmail.com>
2024-04-18 21:34:18 -0300

    Hexagonal Architectur

---
+++ src/main/java/com/services/ms/student/app/infrastructure/adapters/input/rest/model/response/StudentResponse.java
@@ -0,0 +1,19 @@
+package com.services.ms.student.app.infrastructure.adapters.input.rest.model.response;
+
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentResponse {
+
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private Integer age;
+    private String address;
+
+}
